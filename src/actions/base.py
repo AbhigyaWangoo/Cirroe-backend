7a9fab678f1497e8d8924f2c64b945acaf1d09e1
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-06-25 21:49:16 -0700

    Added all base files

---
+++ src/actions/base.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+class BaseUserAction(ABC):
+    """
+    A base class for user actions
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    @abstractmethod
+    def trigger_action(self, input) -> Any:
+        """
+        The standard entrypoint fn to trigger all actions.
+        """
+        pass
