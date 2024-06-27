9bad4433a4942b103fe9962a8b2a330496d3e4ad
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-06-26 21:41:45 -0700

    Added all code for construction phase

--- src/actions/construct.py
+++ src/actions/construct.py
@@ -7,7 +7,8 @@ from src.model.stack import CloudFormationStack
 from include.utils import prompt_with_file, BASE_PROMPT_PATH
 
 CONSTRUCT_CF_PROMPT = "extract_cf_template.txt"
-
+COALESCE_CONSTRUCTION_RESPONSE = "coalesce_response.txt"
+VERIFY_CONSTRUCTED_STACK = "verify_stack.txt"
 
 class ConstructCFStackAction(base.AbstractAction):
     """
@@ -34,16 +35,43 @@ class ConstructCFStackAction(base.AbstractAction):
         Verifies the provided stack is indeed handling the original query's needs or not.
         Also checks the stack against other examples to ensure the syntax is valid.
         """
-        return CloudFormationStack({"": None}, "")
+
+        prompt = f"""
+            Original query:
+            {original_query}
+
+            Constructed stack: 
+            {json.dumps(stack.template)}
+        """
+
+        response = prompt_with_file(
+            VERIFY_CONSTRUCTED_STACK, prompt, self.gpt_client, is_json=True
+        )
+
+        return response
 
     def _coalesce_response(
         self, stack: CloudFormationStack, original_query: str
     ) -> str:
         """
         Respond to the user abstractly in one final response. Ask them whether we should deploy,
-        or whether they'd like to refine the usage.
+        or whether they'd like to refine the usage. Additionlally, if we need any clarifications or 
+        additional info, we need to include that in the response.
         """
-        return ""
+
+        prompt = f"""
+            Original query:
+            {original_query}
+
+            Constructed stack: 
+            {json.dumps(stack.template)}
+        """
+
+        response = prompt_with_file(
+            COALESCE_CONSTRUCTION_RESPONSE, prompt, self.gpt_client
+        )
+
+        return response
 
     def trigger_action(self, infra_description: str) -> Any:
         """
@@ -56,10 +84,9 @@ class ConstructCFStackAction(base.AbstractAction):
         cf_stack = self._extract_template(cleaned_input)
 
         # 3. Check cf stack template against original query.
-        # fixed_cf_stack = self._verify_stack(cf_stack, cleaned_input)
+        fixed_cf_stack = self._verify_stack(cf_stack, cleaned_input)
 
-        # 3.a persist cf stack in storage
+        # 3.a TODO persist cf stack in storage (async ideally)
 
         # 4. Return a string with the detailed info regarding the cf stack's functionality
-        # return self._coalesce_response(fixed_cf_stack, infra_description)
-        return ""
+        return self._coalesce_response(fixed_cf_stack, infra_description)
