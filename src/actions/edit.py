536f01b322926463ab40a75acedac5cc9f672509
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-06-27 19:50:24 -0700

    Implemented edit capabilities

--- src/actions/edit.py
+++ src/actions/edit.py
@@ -1,7 +1,12 @@
 from typing import Any
 from . import base
+import json
 
 from src.model.stack import CloudFormationStack
+from include.utils import BASE_PROMPT_PATH, prompt_with_file
+
+EDIT_STACK_PROMPT = "edit_stack.txt"
+DESCRIBE_EDIT_PROMPT = "describe_edit.txt"
 
 
 class EditCFStackAction(base.AbstractAction):
@@ -17,16 +22,51 @@ class EditCFStackAction(base.AbstractAction):
         """
         Alter the stack_to_edit with the provided user input, and return the new stack.
         """
-        return CloudFormationStack({"": None}, "")
+        new_stack = prompt_with_file(
+            BASE_PROMPT_PATH + EDIT_STACK_PROMPT,
+            user_input,
+            self.gpt_client,
+            is_json=True,
+        )
 
-    def trigger_action(self, input) -> Any:
+        return new_stack
+
+    def describe_changes(
+        self, s1: CloudFormationStack, s2: CloudFormationStack, original_prompt: str
+    ) -> str:
+        """
+        An appropriate response to the user regarding the edit they've made.
+        """
+        diff_prompt = f"""
+        Original infrastructure:
+        {json.loads(s1)}
+        
+        New infrastructure:
+        {json.loads(s2)}
+        
+        User's edit reques:
+        {original_prompt}
+        """
+
+        response = prompt_with_file(
+            BASE_PROMPT_PATH + DESCRIBE_EDIT_PROMPT, diff_prompt, self.gpt_client
+        )
+
+        return response
+
+    def trigger_action(self, input: str) -> Any:
         """
         In this case, triggering an edit involves taking the stack to edit,
         and returning a new one. The input should be the user's desired edit on
-        the existing stack
+        the existing stack.
         """
         # 1. clean input
         new_input = self.clean_input(input)
 
         # 2. Determine new stack with a single gpt call to edit it
-        return self.determine_edit(new_input)
+        edited_stack = self.determine_edit(new_input)
+
+        # 3. Persist new updated stack
+
+        # 4. Respond qualitatively to user
+        return self.describe_changes(self.stack_to_edit, edited_stack, new_input)
