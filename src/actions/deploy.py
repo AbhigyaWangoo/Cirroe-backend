fcaf598efe8f074dc131b44c5505e7110ebe2aec
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-06-30 18:04:46 -0700

    Refined deployment stage further

--- src/actions/deploy.py
+++ src/actions/deploy.py
@@ -1,9 +1,10 @@
-from typing import Any
+from typing import Any, Tuple
 import json
 import boto3
 from . import base
 
 from src.model.stack import CloudFormationStack
+from src.db.supa import SupaClient, ChatSessionState
 
 # TODO use this to validate whether a stack is valid or not before deployment: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudformation/client/validate_template.html
 
@@ -22,6 +23,8 @@ class DeployCFStackAction(base.AbstractAction):
     def __init__(
         self,
         user_stack: CloudFormationStack,
+        chat_session_id: int,
+        state_manager: SupaClient,
         user_aws_secret_key: str,
         user_aws_access_key_id: str,
     ) -> None:
@@ -38,16 +41,25 @@ class DeployCFStackAction(base.AbstractAction):
         )
 
         self.user_stack = user_stack
+        self.state_manager = state_manager
+        self.chat_session_id = chat_session_id
         super().__init__()
 
-    def trigger_action(self, input) -> Any:
+    def verify_stack_deployable(self) -> bool:
+        """
+        A binary classifier that returns whether the stack can be deployed or not.
+        """
+        return True 
+
+    def deploy_stack(self, stack_name: str) -> Tuple[str, ChatSessionState]:
         """
         Deploys user's cf stack into their account
         """
 
-        stack_name = input
         template_body = json.dumps(self.user_stack.template)
         stack_exists = True
+        state: ChatSessionState
+        response = None
 
         try:
             # Check if the stack exists
@@ -63,7 +75,7 @@ class DeployCFStackAction(base.AbstractAction):
 
         if stack_exists:
             # Update the stack
-            print(f"Updating stack {stack_name}...")
+            print(f"Updating stack {stack_name}")
             response = self.cf_client.update_stack(
                 StackName=stack_name,
                 TemplateBody=template_body,
@@ -71,21 +83,50 @@ class DeployCFStackAction(base.AbstractAction):
             )
         else:
             # Create the stack
-            print(f"Creating stack {stack_name}...")
+            print(f"Creating stack {stack_name}")
             response = self.cf_client.create_stack(
                 StackName=stack_name,
                 TemplateBody=template_body,
                 Capabilities=["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM"],
             )
 
+        # Marking the deployment as in progress
+        state = ChatSessionState.DEPLOYMENT_IN_PROGRESS
+        self.state_manager.update_chat_session_state(self.chat_session_id, ChatSessionState.DEPLOYMENT_IN_PROGRESS)
+
         # Wait for the stack to be created/updated
         waiter = self.cf_client.get_waiter(
             "stack_create_complete" if not stack_exists else "stack_update_complete"
         )
         try:
+            print("Waiting on stack deployment...")
             waiter.wait(StackName=stack_name)
             print(f"Stack {stack_name} deployment complete.")
+            state = ChatSessionState.DEPLOYMENT_SUCCEEDED
         except Exception as e:
             print(f"Error waiting for stack deployment: {e}")
+            response += str(e)
+            state = ChatSessionState.DEPLOYMENT_FAILED
+        finally:
+            self.state_manager.update_chat_session_state(self.chat_session_id, state)
 
         return response
+
+    def trigger_action(self, input) -> Any:
+        """
+        Entrypoint for deploying a stack to aws
+        """
+
+        # 1. Check Whether stack can be deployed, according to a previous run. If it 
+        # wasn't then check right now to see deployability.
+        state = self.state_manager.get_cf_stack(self.chat_session_id)
+        deployable = state == ChatSessionState.QUERIED_AND_DEPLOYABLE
+        if not deployable:
+            deployable = self.verify_stack_deployable()
+
+        # 2. if yes, deploy it. Return a deployment report to the user.
+        if deployable:
+            return self.deploy_stack(input)
+
+        # 3. if no, send msg to user asking for remaining info.
+        pass
