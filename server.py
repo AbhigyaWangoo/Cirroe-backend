45b63d9f753c21a45ab9a149b600a2788924aba3
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-07-04 15:28:09 -0700

    Added mocked server for frontend testing

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ load_dotenv()
 
 app = FastAPI()
 
+DEV_MOCK_MSG="Nothing to see here from this query. This is dev, switch to prod domain for actual testing"
+
 # Define request models
 class QueryRequest(BaseModel):
     user_query: str
@@ -23,22 +25,22 @@ class DeployRequest(BaseModel):
 # Synchronous endpoints
 @app.post("/query")
 def query(request: QueryRequest):
-    return {"result": query_wrapper(request.user_query, request.user_id, request.chat_session_id)}
+    return {"result": DEV_MOCK_MSG}
 
 @app.post("/deploy")
 def deploy(request: DeployRequest):
-    return {"result": deploy_wrapper(request.user_id, request.chat_session_id)}
+    return {"result": DEV_MOCK_MSG}
 
 # Asynchronous endpoints
 @app.post("/query_async")
 async def query_async(request: QueryRequest, background_tasks: BackgroundTasks):
-    background_tasks.add_task(query_wrapper, request.user_query, request.user_id, request.chat_session_id)
-    return {"status": "Query task has been started in the background"}
+    # background_tasks.add_task(query_wrapper, request.user_query, request.user_id, request.chat_session_id)
+    return {"status": "Query task has been started in the background (not really. This is dev)"}
 
 @app.post("/deploy_async")
 async def deploy_async(request: DeployRequest, background_tasks: BackgroundTasks):
     background_tasks.add_task(deploy_wrapper, request.user_id, request.chat_session_id)
-    return {"status": "Deploy task has been started in the background"}
+    return {"status": "Deploy task has been started in the background (not really. This is dev)"}
 
 # Main entry point to run the server
 if __name__ == "__main__":
