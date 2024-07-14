3e0f68204e0ed420977aa1c8332e47e53a779502
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-07-13 19:28:00 -0700

    Added dataset obj

--- src/model/stack.py
+++ src/model/stack.py
@@ -9,3 +9,9 @@ class CloudFormationStack:
     def __init__(self, template: Dict[str, Any], name: str) -> None:
         self.name = name
         self.template = template
+class Dataset:
+    """
+    A dataset of cloudformation stacks. Used in fine tuning.
+    """
+    def __init__(self, data: Dict[str, CloudFormationStack]) -> None:
+        self.data = data
