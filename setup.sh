3c9edf1fb8d355a5cd56a0a8156131469d97a482
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-08-17 15:03:28 -0700

    Switched to setup process spawn

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+python3.12 -m venv venv
+source venv/bin/activate
+
+# Install unzip
+apt-get update && apt-get install -y unzip
+
+# Download and install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
+# Verify installation
+aws --version
+
+# Clean up
+rm -rf aws awscliv2.zip
+
+pip3 install -r requirements.txt
