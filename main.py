2775775853b3db389e2a4f58057e301b32091771
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-08-22 18:50:06 -0700

    Fixed trailing dependances

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from uuid import UUID
 
-from src.server.wrappers import query_wrapper, deploy_wrapper, destroy_wrapper
+from src.server.wrappers import query_wrapper
 
 load_dotenv()
 
@@ -17,28 +17,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 # Synchronous endpoints
 @app.get("/query")
 def query(user_query: str, user_id: str, chat_session_id: str):
     user_uuid = UUID(user_id)
     chat_session_uuid = UUID(chat_session_id.strip())
     return {"result": query_wrapper(user_query, user_uuid, chat_session_uuid)}
-
-
-@app.get("/deploy")
-def deploy(user_id: str, chat_session_id: str):
-    user_uuid = UUID(user_id)
-    chat_session_uuid = UUID(chat_session_id.strip())
-    return {"result": deploy_wrapper(user_uuid, chat_session_uuid)}
-
-
-@app.get("/destroy")
-def destroy(user_id: str, chat_session_id: str):
-    user_uuid = UUID(user_id)
-    chat_session_uuid = UUID(chat_session_id.strip())
-    return {"result": destroy_wrapper(user_uuid, chat_session_uuid)}
-
 
 @app.get("/health")
 def test():
